b9d9b9d0b102df90d807c35c47d1e612225ed2a4
Provence <wanghonghust@outlook.com>
2022-09-22 17:24:27 +0800

    新增账号密码界面

--- src/views/tool/ssh/Index.vue
+++ src/views/tool/ssh/Index.vue
@@ -1,10 +1,48 @@
 <template>
   <div></div>
-  <div ref="xterm" class="xterm"/>
+  <el-button type="primary" @click="dialogVisible = true">新增ssh连接</el-button>
+  <el-dialog
+      v-model="dialogVisible"
+      title="新建SSH连接"
+      width="20%"
+      @close="resetForm(ruleFormRef)"
+  >
+    <el-form
+        ref="ruleFormRef"
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        label-width="120px"
+        class="demo-ruleForm"
+    >
+      <el-form-item label="连接名称" prop="name">
+        <el-input v-model="ruleForm.name"/>
+      </el-form-item>
+      <el-form-item label="IP地址" prop="ip">
+        <el-input v-model="ruleForm.ip"/>
+      </el-form-item>
+      <el-form-item label="端口" prop="port">
+        <el-input-number controls-position="right" v-model="ruleForm.port" :min="1"/>
+      </el-form-item>
+      <el-form-item label="用户名" prop="user">
+        <el-input v-model="ruleForm.user"/>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="ruleForm.password" type="password"/>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible=false">取消</el-button>
+        <el-button type="primary" @click="submitForm(ruleFormRef)">确认</el-button>
+      </span>
+    </template>
+  </el-dialog>
+  <div ref="xterm" class="xterm" style="margin-top: 20px"/>
 </template>
 <script lang="ts" setup>
 import 'xterm/css/xterm.css'
-import {onBeforeUnmount, onMounted, ref} from "vue"
+import {onBeforeUnmount, onMounted, reactive, ref} from "vue"
 import {Terminal} from 'xterm'
 import {FitAddon} from 'xterm-addon-fit'
 import {AttachAddon} from 'xterm-addon-attach'
@@ -15,10 +53,13 @@ const xterm = ref(null)
 const term = ref<Terminal>()
 const socketURI = ref('ws://127.0.0.1:8088/tool/ws/1')
 const socket = ref<WebSocket>()
+const dialogVisible = ref(false)
+
 const initTerm = () => {
   const term1 = new Terminal({
     fontSize: 16,
-    cursorBlink: true
+    cursorBlink: true,
+    
   });
   const attachAddon = new AttachAddon(socket.value);
   const fitAddon = new FitAddon();
@@ -34,6 +75,10 @@ const initTerm = () => {
 
 const initSocket = () => {
   socket.value = new WebSocket(socketURI.value);
+  if (socket){
+    socket.value.se
+  }
+
   socketOnClose();
   socketOnOpen();
   socketOnError();
@@ -67,24 +112,24 @@ const socketOnError = () => {
 }
 const al = (event: any) => {
   if (event) {
-    event.returnValue = "我在这写点东西...";
+    event.returnValue = "...";
   }
-  return "我在这写点东西..."
+  return "..."
 }
 onMounted(() => {
   initSocket()
   window.addEventListener('beforeunload', al)
 })
-onBeforeUnmount(() => {
 
+
+onBeforeUnmount(() => {
   socket.value && socket.value.close()
   term.value && term.value.dispose()
-  window.removeEventListener('beforeunload', al)
-
 })
 
 onBeforeRouteLeave((to, from, next) => {
-  ElMessageBox.alert('确定要关闭ssh？', '提示', {confirmButtonText: '确定'})
+  window.removeEventListener('beforeunload', al)
+  ElMessageBox.confirm('离开页面，ssh将断开', '提示', {confirmButtonText: '确定', cancelButtonText: '取消'})
       .then(() => {
         socket.value && socket.value.close()
         term.value && term.value.dispose()
@@ -94,5 +139,39 @@ onBeforeRouteLeave((to, from, next) => {
         next(false)
       });
 })
+
+const ruleFormRef = ref<FormInstance>()
+const ruleForm = reactive({
+  name: '',
+  ip: '',
+  port: 22,
+  user: '',
+  password: '',
+})
+
+const rules = reactive({
+  name: [{required: true, trigger: 'blur', message: "连接名必填"}],
+  ip: [{required: true, trigger: 'blur', message: "ip地址必填"}],
+  port: [{type: 'number', required: true, trigger: 'blur', message: "端口号必填"}],
+  user: [{required: true, trigger: 'blur', message: "用户名必填"}],
+  password: [{required: true, trigger: 'blur', message: "密码必填"}],
+})
+
+const submitForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.validate((valid) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!')
+      return false
+    }
+  })
+}
+
+const resetForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.resetFields()
+}
 
 </script>
